0357c8f500719e766ee4bb199ebbbf3897308a45
lparolari <luca.parolari23@gmail.com>
2021-04-01 11:32:08 +0200

    feat: add styling and analysis type option

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React, { useEffect, useState } from "react";
+import {
+  Container,
+  Jumbotron,
+  Form,
+  Row,
+  Col,
+  Card,
+  Table,
+} from "react-bootstrap";
 
-const getAnomalies = (files) => {
+// const JOINED_ANALYSIS_TYPE = 1;
+// const LEFT_ANALYSIS_TYPE = 2;
+
+const getAnomalies = (files, analysis_type) => {
   let formData = new FormData();
 
   for (let i = 0; i < files.length; i++) {
     formData.append(`file[]`, files[i]);
   }
 
-  return fetch("http://localhost:5000/anomalies/left", {
-    method: "POST",
-    headers: {
-      // "Content-Type": "multipart/form-data"
-    },
-    body: formData,
-  });
+  return fetch(
+    `http://localhost:5000/anomalies?analysis_type=${escape(analysis_type)}`,
+    {
+      method: "POST",
+      body: formData,
+    }
+  );
 };
 
 const responseOrThrow = (response) => {
@@ -30,80 +42,124 @@ const toJson = (response) => {
 
 function App() {
   const [selectedFiles, setSelectedFiles] = useState(undefined);
+  const [selectedAnalysisType, setSelectedAnalysisType] = useState("Left");
   const [data, setData] = useState(undefined);
   const [err, setErr] = useState(undefined);
 
   useEffect(() => {
     selectedFiles &&
-      getAnomalies(selectedFiles)
+      getAnomalies(selectedFiles, selectedAnalysisType)
         .then(responseOrThrow)
         .then(toJson)
         .then(setData)
         .catch(setErr);
-  }, [selectedFiles]);
+  }, [selectedFiles, selectedAnalysisType]);
 
-  // console.log(data);
-  console.log(err);
-
-  if (err) {
-    return (
-      <div>
-        <p>
-          There was an error :( <br />
-          <a
-            href="#"
-            onClick={(e) => {
-              e.preventDefault();
-              setData(undefined);
-              setErr(undefined);
-            }}
-          >
-            back
-          </a>
-        </p>
-        <p>Details: {err.message}</p>
-      </div>
-    );
-  }
+  console.log(data);
 
   return (
-    <div>
-      <h1>Analyze MS Teams Attendance Lists</h1>
+    <Container className="p-3">
+      <Row>
+        <Col>
+          <Jumbotron>
+            <h1 className="header">Analyze MS Teams Attendance Lists</h1>
+          </Jumbotron>
+        </Col>
+      </Row>
 
-      <p>Load your attendance list</p>
-      <form encType="multipart/form-data">
-        <input
-          type="file"
-          multiple
-          onChange={(e) => setSelectedFiles(e.target.files)}
-        />
-      </form>
+      <Row>
+        <Col>
+          <Form encType="multipart/form-data">
+            <Form.Group as={Row}>
+              <Form.Label column sm={2}>
+                Attendance list(s)
+              </Form.Label>
+              <Col sm={10}>
+                <Form.File
+                  id="attendanceLists"
+                  label="Select .csv files"
+                  custom
+                  onChange={(e) => setSelectedFiles(e.target.files)}
+                  multiple
+                  style={{ maxWidth: 280 }}
+                />
+              </Col>
+            </Form.Group>
+            <Form.Group as={Row}>
+              <Form.Label column sm={2}>
+                Analysis type
+              </Form.Label>
+              <Col sm={10}>
+                <Form.Control
+                  as="select"
+                  value={selectedAnalysisType}
+                  onChange={(e) => setSelectedAnalysisType(e.target.value)}
+                  custom
+                  style={{ maxWidth: 280 }}
+                >
+                  <option>Joined</option>
+                  <option>Left</option>
+                </Form.Control>
+              </Col>
+            </Form.Group>
+          </Form>
+        </Col>
+      </Row>
 
-      <div style={{ marginTop: 20 }}>
-        {data &&
-          data.anomalies.map((x, i) => (
-            <div key={`anomalies-${i}`} style={{ marginTop: 20 }}>
-              <table>
-                <thead>
-                  <tr>
-                    <th>Name</th>
-                    <th>Anomaly (+/-mm:ss)</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {x &&
-                    x.outliers.map((x, j) => (
-                      <tr key={`anomaly-${i}-${j}`}>
-                        <td>{x.participant}</td>
-                        <td>{x.delta}</td>
-                      </tr>
-                    ))}
-                </tbody>
-              </table>
-            </div>
-          ))}
-      </div>
-    </div>
+      <Row className="mt-3">
+        <Col md="12">
+          <p>
+            <b>Results</b>
+          </p>
+        </Col>
+        {(err && (
+          <Col md={12}>
+            <p className="text-danger">Oops, there was an error :(</p>
+          </Col>
+        )) || (
+          <Col>
+            <Row>
+              {(!data && (
+                <Col md={12}>
+                  <p>Please select an attendance list</p>
+                </Col>
+              )) ||
+                (data.length == 0 && (
+                  <Col md={12}>
+                    <p>No anomalies found :)</p>
+                  </Col>
+                )) ||
+                data.map((x, i) => (
+                  <Col key={`card-${i}`}>
+                    <Card className="mt-2">
+                      <Card.Header>{x.filename}</Card.Header>
+                      <Card.Body>
+                        <Table>
+                          <thead>
+                            <tr>
+                              <th>Name</th>
+                              <th>Anomaly (+/-mm:ss)</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            {x &&
+                              x.outliers.map((x, j) => (
+                                <tr key={`anomaly-${i}-${j}`}>
+                                  <td>{x.participant}</td>
+                                  <td>{x.delta}</td>
+                                </tr>
+                              ))}
+                          </tbody>
+                        </Table>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                ))}
+            </Row>
+          </Col>
+        )}
+      </Row>
+    </Container>
   );
 }
 
